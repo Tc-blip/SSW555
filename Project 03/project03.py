b3811c3e074041fc44292ef7742d04b45518ab9b
TC <chengtian372@gmail.com>
2019-09-25 16:04:56 -0400

    read person info

--- Project 03/project03.py
+++ Project 03/project03.py
@@ -1,29 +1,105 @@
 from prettytable import PrettyTable
 
+class Person_info:
+    __slots__ = ["ID",'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS']
+
+    def __init__(self,id):
+        self.ID = id
+        self.NAME = ""
+        self.SEX = ""
+        self.BIRT = ""
+        self.DEAT = "NA"
+        self.FAMC = ""
+        self.FAMS = ""
+
+    def add_name(self,name):
+        self.NAME = name
+    
+    def add_sex(self,sex):
+        self.SEX= sex
+    
+    def add_birth(self,birth):
+        self.BIRT = birth
+    
+    def add_deat(self, deat):
+        self.DEAT = deat
+    
+    def add_famc(self,famc):
+        self.FAMC = famc
+    
+    def add_fams(self,fams):
+        self.FAMS = fams
+    
+
 class Individuals:
     __slots__ = ["ID","Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
-    def __init__(self,id,name,gen,bday,age,alive,death,child,spouse):
+    def __init__(self,id):
         self.ID = id
-        self.Name = name
-        self.Gender = gen
-        self.Birthday = bday
-        self.Age = age
-        self.Alive = alive
-        self.Death = death
-        self.Child = child
-        self.Spouse = spouse
-
+        self.Name = ""
+        self.Gender = ""
+        self.Birthday = ""
+        self.Age = ""
+        self.Alive = "True"
+        self.Death = "NA"
+        self.Child = "NA"
+        self.Spouse = "NA"
+    
 
 class Families:
     __slots__ = ["ID", "Married", "Divorced", "Husband_ID", "Husband_Name", "Wife_ID", 
     "Wife_Name", "Children"]
-    def __init__(self, id ,marr,div,hus_id, hus_name, wife_id, wife_name, child):
+    def __init__(self,id):
         self.ID = id
-        self.Married = marr
-        self.Divorced = div
-        self.Husband_ID = hus_id
-        self. Husband_Name = hus_name
-        self.Wife_ID = wife_id
-        self.Wife_Name = wife_name
-        self.Children = child
+        self.Married = "NA"
+        self.Divorced = "NA"
+        self.Husband_ID = "NA"
+        self.Husband_Name = "NA"
+        self.Wife_ID = "NA"
+        self.Wife_Name = "NA"
+        self.Children = "NA"
 
+def file_reader(path):
+    try:
+        fp = open(path, 'r')
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Can't open {path}")
+    else:
+        with fp:
+            for line in fp:
+                line = line.rstrip('\n')
+                yield line
+
+
+pi = {}
+def read_person(path):
+    fp = file_reader(path)
+    
+    for i in fp:
+        new_i = i.split()
+        if new_i[0] == "0":
+            if "INDI" in new_i:
+                pi[new_i[1]] = Person_info(new_i[1])
+                id = new_i[1]
+            if "FAM" in new_i:
+                fm[new_i[1]] = Families(new_i[1])
+                id = new_i[1]
+        else:
+            if new_i[1] == "NAME":
+                pi[id].add_name(" ".join(new_i[2:]))
+            elif new_i[1] == "SEX":
+                pi[id].add_sex(new_i[2])
+            elif new_i[1] == "BIRT":
+                new_i = new_i = next(fp).split()
+                pi[id].add_birth(" ".join(new_i[2:]))
+            elif new_i[1] == "DEAT":
+                new_i = new_i = next(fp).split()
+                pi[id].add_deat(" ".join(new_i[2:]))
+            elif new_i[1] == "FAMC":
+                pi[id].add_famc(new_i[2])
+            elif new_i[1] == "FAMS":
+                pi[id].add_famc(new_i[2])
+        
+           
+if __name__ == "__main__":
+    read_person("test.ged")
+    
