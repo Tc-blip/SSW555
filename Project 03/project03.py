8c74905944108ac50ff01291db43dc70038b4fd9
TC <chengtian372@gmail.com>
2019-09-25 16:23:25 -0400

    add family info

--- Project 03/project03.py
+++ Project 03/project03.py
@@ -56,7 +56,18 @@ class Families:
         self.Husband_Name = "NA"
         self.Wife_ID = "NA"
         self.Wife_Name = "NA"
-        self.Children = "NA"
+        self.Children = []
+
+    def add_marr(self,marr):
+        self.Married = marr
+    def add_husb(self,husb):
+        self.Husband_ID = husb
+    def add_wife(self,wife):
+        self.Wife_ID = wife
+    def add_chil(self,chil):
+        self.Children.append(chil)
+    def add_div(self,div):
+        self.Divorced = div
 
 def file_reader(path):
     try:
@@ -71,6 +82,8 @@ def file_reader(path):
 
 
 pi = {}
+fm = {}
+
 def read_person(path):
     fp = file_reader(path)
     
@@ -98,6 +111,18 @@ def read_person(path):
                 pi[id].add_famc(new_i[2])
             elif new_i[1] == "FAMS":
                 pi[id].add_famc(new_i[2])
+            elif new_i[1] == "MARR":
+                new_i = new_i = next(fp).split()
+                fm[id].add_marr(" ".join(new_i[2:]))
+            elif new_i[1] == "HUSB":
+                fm[id].add_husb(new_i[2])
+            elif new_i[1] == "WIFE":
+                fm[id].add_wife(new_i[2])
+            elif new_i[1] == "CHIL":
+                fm[id].add_chil(new_i[2])
+            elif new_i[1] == "DIV":
+                new_i = new_i = next(fp).split()
+                fm[id].add_div(" ".join(new_i[2:]))
         
            
 if __name__ == "__main__":
