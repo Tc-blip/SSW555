adcdcd7cb85f876a6e74fe234f18b3d2e64f2632
TC <chengtian372@gmail.com>
2019-10-10 00:04:33 -0400

    'test'

--- Project 03/BirthBeforeMorDTest.py
+++ Project 03/BirthBeforeMorDTest.py
@@ -7,10 +7,10 @@ class TestSprint1Methods(unittest.TestCase):
 
     def test_birth_before_marriage_p(self):
         """date1 is earliest date, date2 is later date, date3 is equal to first date"""
-        Lee_birth = datetime.datetime(2015, 1, 1, 0, 0, 0)
-        Lee_marr = datetime.datetime(2016, 1, 1, 0, 0, 0)
-        Lee_marr2 = datetime.datetime(2017, 1, 1, 0, 0, 0)
-        Lee_marr3 = datetime.datetime(1995, 1, 1, 0, 0, 0)
+        Lee_birth = '1 JAN 2015'
+        Lee_marr = '1 JAN 2016'
+        Lee_marr2 = '1 JAN 2017'
+        Lee_marr3 = '1 JAN 1995'
         self.assertTrue(birth_before_marriage_p(Lee_birth, Lee_marr))
         self.assertTrue(birth_before_marriage_p(Lee_birth, Lee_marr2))
         self.assertFalse(birth_before_marriage_p(Lee_birth, Lee_marr3))
@@ -18,10 +18,10 @@ class TestSprint1Methods(unittest.TestCase):
 
     def test_birth_before_death_p(self):
         """date1 is 10 years before the current date, date2 is 5 years before the current date, date3 is 1 year before the current date"""
-        Lee_birth = datetime.datetime(2015, 1, 1, 0, 0, 0)
-        Lee_deth = datetime.datetime(2016, 1, 1, 0, 0, 0)
-        Lee_deth2 = datetime.datetime(2017, 1, 1, 0, 0, 0)
-        Lee_deth3 = datetime.datetime(1995, 1, 1, 0, 0, 0)
+        Lee_birth = '1 JAN 2015'
+        Lee_deth = '1 JAN 2016'
+        Lee_deth2 = '1 JAN 2017'
+        Lee_deth3 = '1 JAN 1995'
 
         self.assertTrue(birth_before_death_p(Lee_birth, Lee_deth))
         self.assertTrue(birth_before_death_p(Lee_birth, Lee_deth2))
