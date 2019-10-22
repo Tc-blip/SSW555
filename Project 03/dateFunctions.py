11df4a16b386925227af43e2ab9bc330273a44b6
liam-king-99 <51165020+liam-king-99@users.noreply.github.com>
2019-10-22 02:40:25 -0400

    Refactoring
    Refactored the files so the functions loop through maps

--- Project 03/dateFunctions.py
+++ Project 03/dateFunctions.py
@@ -9,10 +9,37 @@ def compareDates(date1, date2):
     else:
         return -1
 
-def dateBeforeCurrentDate(date1):
-    if compareDates(date1, dt.datetime.now()) != -1:
-        return False
-    return True
+def dateBeforeCurrentDate(fm, indi):
+    """Returns true if there is an invalid date, false if not"""
+    rightNow = dt.datetime.now()
+    invalidDate = 0
+    for key in fm:
+        family = fm[key]
+        if family.Married != "NA":
+            marriage_date = dt.datetime.strptime(family.Married, '%d %b %Y')
+            if compareDates(marriage_date, rightNow) != -1:
+                print("Error US01: Marriage date of family " + family.ID + " is not before the current date.")
+                invalidDate += 1
+        if family.Divorced != "NA":
+            divorce_date = dt.datetime.strptime(family.Divorced, '%d %b %Y')
+            if compareDates(divorce_date, rightNow) != -1:
+                print("Error US01: Divorce date of family " + family.ID + " is not before the current date.")
+                invalidDate += 1
+
+    for key in indi:
+        individual = indi[key]
+        birth_date = dt.datetime.strptime(individual.Birthday, '%d %b %Y')
+        if compareDates(birth_date, rightNow) != -1:
+            print("Error US01: Birth date of  " + individual.Name + " " + "(" + individual.ID + ") is not before the current date.")
+            invalidDate += 1
+        if individual.Death != "NA":
+            death_date = dt.datetime.strptime(individual.Death, '%d %b %Y')
+            if compareDates(death_date, rightNow) != -1:
+                print("Error US01: Death date of  " + individual.Name + " " + "(" + individual.ID + ") is not before the current date.")
+                invalidDate += 1
+
+    return invalidDate != 0
+
 
 def differenceBetweenDates(date1, date2):
     '''Takes in 2 dates and returns date1-date2 in years'''
@@ -21,8 +48,21 @@ def differenceBetweenDates(date1, date2):
     years = (((delta/3600)/24)/365)
     return years
 
-def lessThan150YearsOld(date1):
-    if differenceBetweenDates(date1, dt.datetime.now()) < 150:
-        return True
-    else:
-        return False
+def lessThan150YearsOld(indi):
+    """Returns true if all relevant dates are good, false if not"""
+    allDatesGood = 0
+    rightNow = dt.datetime.now()
+    for key in indi:
+        individual = indi[key]
+        birth_date = dt.datetime.strptime(individual.Birthday, '%d %b %Y')
+        if differenceBetweenDates(rightNow, birth_date) > 150:
+            if individual.Death != "NA":
+                death_date = dt.datetime.strptime(individual.Death, '%d %b %Y')
+                if differenceBetweenDates(death_date, birth_date) >= 150:
+                    print("Error US07: Death date of  " + individual.Name + " " + "(" + individual.ID + ") is at least 150 years after birth.")
+                    allDatesGood += 1
+            else:
+                if differenceBetweenDates(rightNow, birth_date) >= 150:
+                    print("Error US07: Birth date of  " + individual.Name + " " + "(" + individual.ID + ") is at least 150 years ago and " + individual.Name + " is still alive.")
+                    allDatesGood += 1
+    return allDatesGood == 0
