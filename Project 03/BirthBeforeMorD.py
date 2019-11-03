b1ac3d9b52f29360296539039c383587e571b595
TC <chengtian372@gmail.com>
2019-11-03 16:04:29 -0500

    Update BirthBeforeMorD.py

--- Project 03/BirthBeforeMorD.py
+++ Project 03/BirthBeforeMorD.py
@@ -23,13 +23,13 @@ def check_Birth_before_marr(fm,pi):
         hus_birthday = pi[i.Husband_ID].BIRT
         if i.Married != 'NA':
             if not birth_before_marriage_p(hus_birthday,i.Married):
-                print(f'Error {i.Husband_Name} birthday_before_marr birthday {hus_birthday}  married {i.Married}')
+                print(f'US02 Error {i.Husband_Name} birthday_before_marr birthday {hus_birthday}  married {i.Married}')
         
         #check wife birthday before marr
         if i.Wife_ID != 'NA':
             wife_birthday = pi[i.Wife_ID].BIRT
             if not birth_before_marriage_p(wife_birthday,i.Married):
-                print(f'Error {i.Wife_Name} birthday_before_marr birthday {wife_birthday}  married {i.Married}')
+                print(f'US02 Error {i.Wife_Name} birthday_before_marr birthday {wife_birthday}  married {i.Married}')
       
 
 def birth_before_marriage_p(date1,date2):
@@ -45,7 +45,7 @@ def check_Birth_before_death(indi):
     for i in indi.values():
         if i.Death != 'NA':
             if not birth_before_death_p(i.Birthday,i.Death):
-                print(f'Error {i.Name} birthday_before_death birthday  {i.Birthday}  Death {i.Death}')
+                print(f'US03 Error {i.Name} birthday_before_death birthday  {i.Birthday}  Death {i.Death}')
 
 
 def birth_before_death_p(date1,date2):
