7a46b77cc1b71aa43d7042ef5b42b86d1ae00b39
liam-king-99 <51165020+liam-king-99@users.noreply.github.com>
2019-11-03 20:13:50 -0500

    Changed print statement

--- Project 03/US29.py
+++ Project 03/US29.py
@@ -3,6 +3,6 @@ def listDeceased(indi):
     for key in indi:
         individual = indi[key]
         if individual.Alive == "False":
-            print("Individual " + individual.ID + " is dead.")
+            print("US29: Individual " + individual.ID + " is dead.")
             deadIDs.append(individual.ID)
     return deadIDs
