6e2f588a387bc8a9d3ab2e38292ca37fcbd89d74
cy <cylee820621@gmail.com>
2019-11-06 09:36:12 -0500

    add gedcom

--- Project 03/DivorceBeforeDeath.py
+++ Project 03/DivorceBeforeDeath.py
@@ -20,9 +20,9 @@ def check_divorce_before_death(fm, pi):
         if check == 1:
             print(f"ERROR: FAMILY: US06: {family.ID} wife {family_wife_ID} died before devorced date!")
         elif check == 2:
-            print(f"ERROR: FAMILY: US06: {family.ID} wife {family_husban_ID} died before devorced date!")
+            print(f"ERROR: FAMILY: US06: {family.ID} husban {family_husban_ID} died before devorced date!")
         elif check == 3:
-            print(f"ERROR: FAMILY: US06: {family.ID} wife {family_husban_ID} and husban {family_husban_ID} died before devorced date!")
+            print(f"ERROR: FAMILY: US06: {family.ID} wife {family_wife_ID} and husban {family_husban_ID} died before devorced date!")
 
 def divorce_before_death(family_divorce_date, husban_death_date, wife_death_date):
     # return 1 means wife died before marriage!
--- Project 03/MultipleBirths.py
+++ Project 03/MultipleBirths.py
@@ -1,6 +1,6 @@
 """
 Autor: CHIH-YU LEE
-User story 13
+User story 14
 No more than five siblings should be born at the same time
 """
 import datetime
@@ -12,7 +12,7 @@ def multiple_birth(fm,pi):
         child_birth_dict = check_children_birthday(children,pi)
         check = check_if_more_than_5_children(child_birth_dict)
         if check == False:
-            print(f'ERROR: FAMILY: US13: {family.ID} has more than five siblings born at the same time!')
+            print(f'ERROR: FAMILY: US14: {family.ID} has more than five siblings born at the same time!')
 
 def check_children_birthday(children,pi):
     child_birth_dict = {}
@@ -27,7 +27,7 @@ def check_children_birthday(children,pi):
 def check_if_more_than_5_children(child_birth_dict):
     if child_birth_dict:
         for bithday_children_num in child_birth_dict.values():
-            if not bithday_children_num <= 5:
+            if bithday_children_num > 5:
                 return False
             else:
                 return True
