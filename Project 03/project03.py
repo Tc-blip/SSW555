2a10525c2f0c8021a5707b2c28eea3a4f0bfbbbb
Jia Wen <jwen6@stevens.edu>
2019-11-19 17:51:03 -0500

    Update project03.py

--- Project 03/project03.py
+++ Project 03/project03.py
@@ -242,11 +242,14 @@ def pt_id():
 
 
 class TestUserStory(unittest.TestCase):
+    #tests for user story 20 & 26
     
     def test_check_Aunt_and_Uncles(self):
+        '''US 20 test '''
         self.assertEqual(check_Aunt_and_Uncles(fm,indi), {'@F9@'})
 
     def test_check_Corresponding_Entries(self):
+        ''' US 26 test'''
         self.assertEqual(check_Corresponding_Entries(fm, indi), {'@I15@', '@I14@'})
 
 
@@ -301,4 +304,4 @@ if __name__ == "__main__":
 
     check_Aunt_and_Uncles(fm,indi)                      #20 --sprint 4
     check_Corresponding_Entries(fm,indi)                #26 --sprint 4
-    unittest.main()
+    unittest.main()                                     #20&26 test --sprint 4
